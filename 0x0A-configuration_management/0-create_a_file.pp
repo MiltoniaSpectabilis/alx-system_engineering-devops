57dc60ce6dccc231506b1e0526dfcbc46c1bf2e2
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-06-29 00:46:50 +0100

    add comment

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,10 @@
+# This Puppet manifest creates a file at /tmp/school with specific properties:
+# - File path: /tmp/school
+# - File permission: 0744
+# - File owner: www-data
+# - File group: www-data
+# - File content: "I love Puppet"
+
 file { '/tmp/school':
   ensure  => file,
   content => 'I love Puppet',
