9a417f34ce48972349314c52426f74c0803a9c7a
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-14 03:03:21 +0100

    another attempt

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"
-    }
-
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Edge109.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json()['data']['subscribers']
