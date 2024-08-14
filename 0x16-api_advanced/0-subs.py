9b385ffbb61720e348c95167e9d85534d1fbcfdc
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-14 03:42:11 +0100

    last attempt

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""
-Module to query Reddit API for subscriber count.
-"""
+"""Module to query Reddit API for subscriber count."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers for a given subreddit."""
+    """Return number of subscribers for a subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0"}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        return response.json()['data']['subscribers']
+        data = response.json()
+        return data['data']['subscribers']
     return 0
