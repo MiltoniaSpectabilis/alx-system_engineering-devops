17afe919d11de04354051a899a9e22c6a7b58e23
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-15 01:33:36 +0100

    add comment

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Replace 'phpp' with 'php' in /var/www/html/wp-settings.php
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
