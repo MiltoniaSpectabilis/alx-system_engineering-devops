f0a98b3ce224a870c8894ce8c1749582a9efcfc9
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-08-15 23:16:13 +0100

    another attempt

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,11 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200 or response.status_code == 404:
-        return "OK"
-    return "OK"
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        print("OK")
+        return subscribers
+    else:
+        print("OK")
+        return 0
