2d440305d44827f7553e06bf12be286b28ecab23
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-09-18 22:34:44 +0100

    quick test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Retrieves the subscriber count of a given subreddit."""
+"""Returns the number of subscribers of a subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the subscriber count, 0 if invalid subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Edge109.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json()['data']['subscribers']
-    return 0
+    if response.status_code != 200:
+        return 0
+    return response.json()['data']['subscribers']
