46397d54879a7b90c6f7a5e5dd19985693810903
MiltoniaSpectabilis <125466560+MiltoniaSpectabilis@users.noreply.github.com>
2025-06-30 04:43:43 +0100

    Shorten docstring

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-"""This script fetches data from an API
-and displays it in specified format
+"""API Exercise
 """
 
 import requests
@@ -13,14 +12,14 @@ if __name__ == '__main__':
     names_url = f'https://jsonplaceholder.typicode.com/users/{id}'
     tasks_data = requests.get(tasks_url).json()
     names_data = requests.get(names_url).json()
-# get completed tasks
+    # get completed tasks
     completed_tasks = [task.get('title')
                        for task in tasks_data if task.get('completed')]
-# get user name
+    # get user name
     user_name = names_data.get('name')
-# calculate num of completed_tasks
+    # calculate num of completed_tasks
     num_completed_tasks = len(completed_tasks)
-# calculate num of all tasks
+    # calculate num of all tasks
     num_all_tasks = len(tasks_data)
     print(f'Employee {user_name} is done with tasks({
         num_completed_tasks}/{num_all_tasks}):')
