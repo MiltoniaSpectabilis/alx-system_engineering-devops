f611168a5a0798ecce9ba5c7b586f82d1b7d720b
MiltoniaSpectabilis <125466560+MiltoniaSpectabilis@users.noreply.github.com>
2025-06-30 04:38:13 +0100

    Add main guard

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""
-This script fetches data from an API
+"""This script fetches data from an API
 and displays it in specified format
 """
 
 import requests
 from sys import argv
 
-id = argv[1]
-tasks_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-names_url = f'https://jsonplaceholder.typicode.com/users/{id}'
-tasks_data = requests.get(tasks_url).json()
-names_data = requests.get(names_url).json()
+if __name__ == '__main__':
+    id = argv[1]
+    tasks_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    names_url = f'https://jsonplaceholder.typicode.com/users/{id}'
+    tasks_data = requests.get(tasks_url).json()
+    names_data = requests.get(names_url).json()
 # get completed tasks
-completed_tasks = [task.get('title')
-                   for task in tasks_data if task.get('completed')]
+    completed_tasks = [task.get('title')
+                       for task in tasks_data if task.get('completed')]
 # get user name
-user_name = names_data.get('name')
+    user_name = names_data.get('name')
 # calculate num of completed_tasks
-num_completed_tasks = len(completed_tasks)
+    num_completed_tasks = len(completed_tasks)
 # calculate num of all tasks
-num_all_tasks = len(tasks_data)
-print(f'Employee {user_name} is done with tasks({
-    num_completed_tasks}/{num_all_tasks}):')
-for c_task in completed_tasks:
-    print('\t', c_task)
+    num_all_tasks = len(tasks_data)
+    print(f'Employee {user_name} is done with tasks({
+        num_completed_tasks}/{num_all_tasks}):')
+    for c_task in completed_tasks:
+        print('\t', c_task)
