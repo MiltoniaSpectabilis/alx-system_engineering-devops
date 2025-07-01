9889c6a40135e81696dfc201ad1d87bedf2e354a
MiltoniaSpectabilis <125466560+MiltoniaSpectabilis@users.noreply.github.com>
2025-07-01 01:30:33 +0100

    Implement a py script that fetches data from an API and stores in a csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""API Exercise
+"""
+
+import requests
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    id = argv[1]
+    tasks_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
+    names_url = f'https://jsonplaceholder.typicode.com/users/{id}'
+    tasks_data = requests.get(tasks_url).json()
+    names_data = requests.get(names_url).json()
+
+    user_name = names_data.get('username')
+
+    csv_filename = f'{id}.csv'
+
+    with open(csv_filename, 'w', newline='', encoding='utf-8') as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in tasks_data:
+            csv_writer.writerow([
+                id,
+                user_name,
+                task.get('completed'),
+                task.get('title')
+            ])
